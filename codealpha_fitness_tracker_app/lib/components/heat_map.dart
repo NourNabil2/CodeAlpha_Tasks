e30ddf7b0673ee5806dfc83964b4b3b3d9cb0ef3
NourNabil2 <nour60g@gmail.com>
2024-03-26 20:55:21 +0200

    fitness tracker app

---
+++ codealpha_fitness_tracker_app/lib/components/heat_map.dart
@@ -0,0 +1,37 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_heatmap_calendar/flutter_heatmap_calendar.dart';
+
+import '../datetime/date_time.dart';
+
+class MyHeatMap extends StatelessWidget {
+  final Map<DateTime, int>? datasets;
+  final String startDateYYYYMMDD;
+  const MyHeatMap({
+    super.key,
+    required this.datasets,
+    required this.startDateYYYYMMDD,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(25),
+      child: HeatMap(
+        startDate: createDateTimeObject(startDateYYYYMMDD),
+        endDate: DateTime.now().add(const Duration(days: 0)),
+        datasets: datasets,
+        colorMode: ColorMode.color,
+        defaultColor: Colors.grey,
+        textColor: Colors.black,
+        showColorTip: false,
+        showText: true,
+        scrollable: true,
+        size: 40,
+        colorsets: const {
+          1: Colors.teal,
+        },
+      ),
+    );
+  }
+}
